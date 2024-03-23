02dab7fcb62dca2b2fe97033cac06299d93a878d
josephperez956 <josephperez@yahoo.com>
2024-03-22 19:03:54 -0500

    added input Content and Categories

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,21 @@
 <script>
 import {ref} from 'vue'
 const name = ref('')
+const todoList = ref([])
+const inputContent = ref('')
+const inputCategory = ref(null)
+const addTodoList = () => {
+     if(inputContent.value.trim()==='' || inputCategory.value == null) {
+          return
+     }
+     todoList.value.push({
+          content: inputContent.value,
+          category: inputCategory = n,
+     })
+     inputContent.value = ''
+     inputCategory.value = ''
+     console.log(todoList)
+}
 </script>
 
 <template>
@@ -12,7 +27,29 @@ const name = ref('')
                </h2>
           </section>
           <section class="create-todo">
+               <h3>CREATE A TO DO LIST</h3>
+               <form @submit.prevent="addTodoList">
+                    <h4>What's on your to do list?</h4>
+                    <input type="text" placeholder="e.g. Make a Video" v-model="inputContent" />
+                    {{  inputContent }}
 
+                    <h4>Pick a Category</h4>
+                    <div class="options">
+                         <label>
+                              <input type = "radio" name="category" value="business" v-model="inputCategory" />
+                              <span class="bubble business"></span>
+                              <div>Business</div>
+                         </label>
+
+                         <label>
+                              <input type = "radio" name="category" value="personal" v-model="inputCategory" />
+                              <span class="bubble personal"></span>
+                              <div>Personal</div>
+                         </label>
+
+                         {{ inputCategory }}
+                    </div>
+               </form>
           </section>
           <section class="todo-list">
 
